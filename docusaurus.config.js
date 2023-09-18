3aaec9fc31d2afef94a08b162d527b914dc2145d
Styler <styler@streamelements.com>
2023-09-19 01:03:52 +0200

    Update production URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://docs.streamelements.dev',
+  url: 'https://docs.streamelements.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
