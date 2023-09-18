a8b80c9c77db135baca6bfd7699a07e5ec523f92
Styler <styler@streamelements.com>
2023-09-18 17:23:33 +0200

    Update sidebar positions and navigation link in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
