c87f0b2a87a970ae07652d3648c55e0b430ec2e0
Styler <styler@streamelements.com>
2023-09-24 17:20:16 +0200

    Update StreamElements Docs metadata keywords

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
         title: 'StreamElements Docs',
       },
       metadata: [
-        {name: 'keywords', content: 'Streaming,Chatbot,documentation,StreamElements'},
+        {name: 'keywords', content: 'Streaming,Chatbot,documentation,StreamElements,Live Streaming,Guides,Tutorials,Help,Support,FAQ,How-to,Stream Setup,Stream Tools,StreamElements Features,StreamElements Services'},
         {name: 'twitter:card', content: 'summary_large_image'},
       ],
       colorMode: {
